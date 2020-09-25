361f94fe67e8998effeaa22e19da6f8b926a4ee6
apushkin08 <apushkin08@gmail.com>
2020-09-26 00:27:05 +0300

    add tests

--- cmd/internal/app/apiserver/config.go
+++ cmd/internal/app/apiserver/config.go
@@ -1,13 +1,15 @@
 package apiserver
 
-// Congig ...
+// Config ...
 type Config struct {
-	BindAddr string `toml:"bind_adr"`
+	BindAddr string `toml:"bind_addr"`
+	LogLevel string `toml:"log_level"`
 }
 
 // NewConfig ...
 func NewConfig() *Config {
 	return &Config{
 		BindAddr: ":8080",
+		LogLevel: "debug",
 	}
 }

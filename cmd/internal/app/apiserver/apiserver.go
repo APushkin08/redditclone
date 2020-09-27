6ced1a022532cf7b44c433358ee56a7e8cbe9028
apushkin08 <apushkin08@gmail.com>
2020-09-27 11:34:03 +0300

    add store

--- cmd/internal/app/apiserver/apiserver.go
+++ cmd/internal/app/apiserver/apiserver.go
@@ -4,6 +4,7 @@ import (
 	"io"
 	"net/http"
 
+	"github.com/apushkin08/redditclone/cmd/internal/app/store"
 	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
 )
@@ -13,6 +14,7 @@ type APIServer struct {
 	config *Config
 	logger *logrus.Logger
 	router *mux.Router
+	store  *store.Store
 }
 
 // New ...
@@ -31,6 +33,10 @@ func (s *APIServer) Start() error {
 	}
 
 	s.configureRouter()
+
+	if err := s.configureStore(); err != nil {
+		return err
+	}
 
 	s.logger.Info("starting api server")
 
@@ -52,6 +58,18 @@ func (s *APIServer) configureLogger() error {
 // configureRouter ...
 func (s *APIServer) configureRouter() {
 	s.router.HandleFunc("/hello", s.handleHello())
+}
+
+// configureStore ...
+func (s *APIServer) configureStore() error {
+	st := store.New(s.config.Store)
+	if err := st.Open(); err != nil {
+		return err
+	}
+
+	s.store = st
+
+	return nil
 }
 
 func (s *APIServer) handleHello() http.HandlerFunc {
---
+++ cmd/internal/app/store/config.go
@@ -0,0 +1,12 @@
+package store
+
+// Config ...
+type Config struct {
+	DataBaseURL string `toml:"database_url"`
+}
+
+// NewConfig ...
+func NewConfig() *Config {
+	return &Config{}
+
+}
